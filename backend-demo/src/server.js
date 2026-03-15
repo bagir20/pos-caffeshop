decd48100b2192a3b1d4ee4fa75542a49bd9e766
Muhammad Bagir <mdbagir20@gmail.com>
2026-03-15 13:33:40 +0700

    fix: replace wildcard * with (.*) for express options

--- backend-demo/src/server.js
+++ backend-demo/src/server.js
@@ -32,7 +32,7 @@ app.use(cors({
   credentials: true
 }));
 
-app.options('*', cors({
+app.options('(.*)', cors({
   origin: function(origin, callback) {
     if (!origin || origin.match(/https:\/\/pos-caffeshop-u9m5.*\.vercel\.app/)) {
       callback(null, true);
